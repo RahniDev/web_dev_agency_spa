465390a39c98ab3939f0cff094d48c908053ac12
RahniDev <rahni32@protonmail.com>
2024-10-06 17:27:40 +0200

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import Hero from './components/Hero'
+import Services from './components/Services'
+import Work from './components/Work'
+import Contact from './components/Contact'
 
 const App = () => {
-
   return (
     <>
       <h1>Web Development Agency</h1>
+      <Hero />
+      <Services />
+      <Work />
+      <Contact />
     </>
   )
 }
